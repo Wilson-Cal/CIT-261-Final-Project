86d974abe3a68d18d43e24ab22721636ad4e0c53
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2018-07-01 02:33:43 -0600

    Basic functionality started

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ let storeData;
 window.addEventListener('load', () => {
     // Make the homepage
 
-    // First load the store from local store or GET it from Github
-    
-    if(!localStorage.getItem('store')) {
-        console.log('hello');
-        getProducts().then(data => {
-            writeLocal('store', data);
-            storeData = data;
-        });
-    } else {
-        storeData = readLocal('store');
-    }
-    content.appendChild(createH2Tag('Products'));
-    for(let category in storeData.products) {
-        content.appendChild(createPTag(storeData.products[category]));
-    }
-	
+    // First load the store from Github
+    getProducts().then(data => {
+        storeData = data;
+        content.appendChild(createH2Tag('Products'));
+        for(let category in storeData.products) {
+            content.appendChild(createPTag(storeData.products[category][0].name));
+            content.appendChild(createPTag('$' + storeData.products[category][0].price[0]));
+        }
+    });
 });
