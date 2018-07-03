7085fe0f4f42e9f0db5d3e954de47d1a3d0c423a
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2018-07-02 23:40:38 -0600

    Added JSON files for PC parts. Switched to PC Maker.

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-let content = getContent(),
-    footer = getFooter();
+let pageHeader = getPageHeader();
+let navBar = getNavBar();
+let content = getContent();
+let footer = getFooter();
 let storeData;
 
 window.addEventListener('load', () => {
-    // Make the homepage
-
-    // First load the store from Github
-    getProducts().then(data => {
-        storeData = data;
-        content.appendChild(createH2Tag('Products'));
-        for(let category in storeData.products) {
-            content.appendChild(createPTag(storeData.products[category][0].name));
-            content.appendChild(createPTag('$' + storeData.products[category][0].price[0]));
-        }
-    });
 });
